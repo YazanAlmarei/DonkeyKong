423a2acf61f4583e1a3f56bfef0758e72b0f4c14
Yazan Almarei <yazanalmarei@hotmail.com>
2022-10-26 23:52:35 +0200

    commit

--- DonkeyKong/Game1.cs
+++ DonkeyKong/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace DonkeyKong
 {
@@ -8,7 +11,10 @@ namespace DonkeyKong
     {
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-        
+
+
+
+        Tile[,] tiles;
 
 
         public Game1()
@@ -16,8 +22,8 @@ namespace DonkeyKong
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-            graphics.PreferredBackBufferWidth = 1025;
-            graphics.PreferredBackBufferHeight = 650;
+            graphics.PreferredBackBufferWidth = 1000;
+            graphics.PreferredBackBufferHeight = 663;
 
         }
 
@@ -32,7 +38,49 @@ namespace DonkeyKong
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            Texture2D bridgeTileTex = Content.Load<Texture2D>("bridge");
+            Texture2D bridgeLadderTex = Content.Load<Texture2D>("bridgeLadder");
+            Texture2D emptyTex = Content.Load<Texture2D>("empty");
+            Texture2D ladderTex = Content.Load<Texture2D>("Ladder");
+
+            List<string> strings = new List<string>();
+            StreamReader sr = new StreamReader("mapTXT.txt");
+            while (!sr.EndOfStream)
+            {
+                strings.Add(sr.ReadLine());
+            }
+
+            sr.Close();
+
+            tiles = new Tile[strings[0].Length, strings.Count];
+
+            for (int i = 0; i < tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < tiles.GetLength(1); j++)
+                {
+
+                    if (strings[j][i] == 'B')
+                    {
+                        tiles[i, j] = new Tile(bridgeTileTex, new Vector2(bridgeTileTex.Width * i, bridgeTileTex.Height * j), false);
+                    }
+
+                    else if (strings[j][i] == 'C')
+                    {
+                        tiles[i, j] = new Tile(bridgeLadderTex, new Vector2(bridgeLadderTex.Width * i, bridgeLadderTex.Height * j), false);
+                    }
+
+                    else if (strings[j][i] == 'E')
+                    {
+                        tiles[i, j] = new Tile(emptyTex, new Vector2(emptyTex.Width * i, emptyTex.Height * j), false);
+                    }
+
+                    else if (strings[j][i] == 'L')
+                    {
+                        tiles[i, j] = new Tile(ladderTex, new Vector2(ladderTex.Width * i, ladderTex.Height * j), false);
+                    }
+                }
+            }
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -48,6 +96,18 @@ namespace DonkeyKong
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+
+            for (int i = 0; i < tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < tiles.GetLength(1); j++)
+                {
+                    tiles[i, j].Draw(spriteBatch);
+                }
+            }
+
+
+            spriteBatch.End();
 
             // TODO: Add your drawing code here
 
---
+++ DonkeyKong/Tile.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonkeyKong
+{
+    public class Tile
+    {
+        public Vector2 bridgePos;
+        public Texture2D bridgeTileTex;
+        public bool bridgee;
+
+        public Tile(Texture2D bridgeTileTex, Vector2 bridgePos, bool bridgee)
+        {
+            this.bridgeTileTex = bridgeTileTex;
+            this.bridgePos = bridgePos;
+            this.bridgee = bridgee;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(bridgeTileTex, bridgePos, Color.White);
+        }
+
+
+    }
+}
